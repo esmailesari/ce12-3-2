7e1018b9d539763bbeb05cc4bd88d504d5095efc
esmailesari <esmailesari@gmail.com>
2026-05-07 20:08:31 +0800

    Adding new S3 Bucket

---
+++ main.tf
@@ -0,0 +1,34 @@
+# 1. Provider Configuration
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "sctp-ce12-tfstate-bucket"
+    key            = "ee-s3buckets.tfstate"
+    region         = "ap-southeast-1"
+  }
+}
+
+# 3. S3 Bucket Resource
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "ee-s3-bucket-3-1" # Ensure this is globally unique
+
+  tags = {
+    Name        = "EE S3 Bucket"
+  }
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.44.0"
+    }
+  }
+}
+
+terraform {
+  required_version = ">= 1.5.0, < 2.0.0"
+}
